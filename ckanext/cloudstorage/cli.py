be04005c89ed679d71f9798d9f2e9e00e2946d1e
JVickery-TBS <jesse.vickery@tbs-sct.gc.ca>
2023-04-24 15:56:24 +0000

    feat(commands): added commands to help manage remote uploads;
    - Added `list-linked-uploads` command.
    - Added `list-unlinked-uploads` command.
    - Added `remove-unlinked-uploads` command.
    - Added `list-missing-uploads` command.

--- ckanext/cloudstorage/cli.py
+++ ckanext/cloudstorage/cli.py
@@ -1,4 +1,5 @@
 import click
+from typing import Union
 
 from ckanext.cloudstorage import utils
 
@@ -18,3 +19,41 @@ def cloudstorage():
 @cloudstorage.command()
 def initdb():
     utils.initdb()
+
+
+@cloudstorage.command()
+@click.option(
+    "-o",
+    "--output",
+    default=None,
+    help="The output file path.",
+)
+def list_unlinked_uploads(output: Union[str, None]):
+    utils.list_linked_uploads(output)
+
+
+@cloudstorage.command()
+def remove_unlinked_uploads():
+    utils.remove_unlinked_uploads()
+
+
+@cloudstorage.command()
+@click.option(
+    "-o",
+    "--output",
+    default=None,
+    help="The output file path.",
+)
+def list_missing_uploads(output: Union[str, None]):
+    utils.list_missing_uploads(output)
+
+
+@cloudstorage.command()
+@click.option(
+    "-o",
+    "--output",
+    default=None,
+    help="The output file path.",
+)
+def list_linked_uploads(output: Union[str, None]):
+    utils.list_linked_uploads(output)
--- ckanext/cloudstorage/commands.py
+++ ckanext/cloudstorage/commands.py
@@ -21,17 +21,26 @@ from ckan.logic import NotFound
 USAGE = """ckanext-cloudstorage
 
 Commands:
-    - fix-cors       Update CORS rules where possible.
-    - migrate        Upload local storage to the remote.
-    - initdb         Reinitalize database tables.
+    - fix-cors                  Update CORS rules where possible.
+    - migrate                   Upload local storage to the remote.
+    - initdb                    Reinitalize database tables.
+    - list-unlinked-uploads     Lists uploads in the storage container that do not match to any resources.
+    - remove-unlinked-uploads   Permanently deletes uploads from the storage container that do not match to any resources.
+    - list-missing-uploads      Lists resources IDs that are missing uploads in the storage container.
+    - list-linked-uploads       Lists uploads in the storage container that do match to a resource.
 
 Usage:
     cloudstorage fix-cors <domains>... [--c=<config>]
     cloudstorage migrate <path_to_storage> [<resource_id>] [--c=<config>]
     cloudstorage initdb [--c=<config>]
+    cloudstorage list-unlinked-uploads [--o=<output>] [--c=<config>]
+    cloudstorage remove-unlinked-uploads [--c=<config>]
+    cloudstorage list-missing-uploads [--o=<output>] [--c=<config>]
+    cloudstorage list-linked-uploads [--o=<output>] [--c=<config>]
 
 Options:
     -c=<config>       The CKAN configuration file.
+    -o=<output>       The output file path.
 """
 
 
@@ -45,6 +54,11 @@ class PasterCommand(CkanCommand):
     summary = 'ckanext-cloudstorage maintence utilities.'
     usage = USAGE
 
+    def __init__(self, name):
+        super(PasterCommand, self).__init__(name)
+        self.parser.add_option('-o', '--output', dest='output', action='store',
+                               default=None, help='The output file path.')
+
     def command(self):
         self._load_config()
         args = docopt(USAGE, argv=self.args)
@@ -55,6 +69,14 @@ class PasterCommand(CkanCommand):
             _migrate(args)
         elif args['initdb']:
             _initdb()
+        elif args['list-unlinked-uploads']:
+            _list_unlinked_uploads(self.options.output)
+        elif args['remove-unlinked-uploads']:
+            _remove_unlinked_uploads()
+        elif args['list-missing-uploads']:
+            _list_missing_uploads(self.options.output)
+        elif args['list-linked-uploads']:
+            _list_linked_uploads(self.options.output)
 
 
 def _migrate(args):
@@ -166,3 +188,22 @@ def _fix_cors(args):
 
 def _initdb():
     utils.initdb()
+
+
+def _list_unlinked_uploads(output_path):
+    # type: (str|None) -> None
+    utils.list_unlinked_uploads(output_path)
+
+
+def _remove_unlinked_uploads():
+    utils.remove_unlinked_uploads()
+
+
+def _list_missing_uploads(output_path):
+    # type: (str|None) -> None
+    utils.list_missing_uploads(output_path)
+
+
+def _list_linked_uploads(output_path):
+    # type: (str|None) -> None
+    utils.list_linked_uploads(output_path)
